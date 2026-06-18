ad43163f4d28fd68b3eac4919d49c4c04e5a7bec
Alan1203TI <167146297+Alan1203TI@users.noreply.github.com>
2026-06-18 11:26:06 -0300

    Delete firebase-init.js

--- firebase-init.js
+++
@@ -1,6 +0,0 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
-import { firebaseConfig } from "./firebase-config.js";
-
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
